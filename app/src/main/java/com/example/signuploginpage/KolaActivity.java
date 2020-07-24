532356897f42b2aeba175e55215bbd1c37489f2c
dekola <akano.adekola@gmail.com>
2020-07-24 23:31:49 +0100

    A

---
+++ app/src/main/java/com/example/signuploginpage/KolaActivity.java
@@ -0,0 +1,14 @@
+package com.example.signuploginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KolaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kola);
+    }
+}
