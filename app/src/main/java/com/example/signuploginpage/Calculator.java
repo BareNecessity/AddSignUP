aada1b2a0eee034c88628326874df8ba5aae620a
Bare Necessities <oluwatosinJAjao@outlook.com>
2020-07-27 14:12:52 -0700

    Added operation click to all operators

--- app/src/main/java/com/example/signuploginpage/Calculator.java
+++ app/src/main/java/com/example/signuploginpage/Calculator.java
@@ -2,6 +2,7 @@ package com.example.signuploginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,14 +16,28 @@ public class Calculator extends AppCompatActivity {
         setContentView(R.layout.activity_calculator);
     }
 
-    public void operationClick(View view){
-        if (view.getId() == R.id.Multiplication){
+    public void operationClick(View view) {
+        if (view.getId() == R.id.Multiplication) {
             Toast.makeText(this, "Clicked on multiplication", Toast.LENGTH_LONG).show();
-        }
-        else if (view.getId() == R.id.Addition){
+        } else if (view.getId() == R.id.Addition) {
             Toast.makeText(this, "Clicked on addition", Toast.LENGTH_LONG).show();
+        } else if (view.getId() == R.id.ClearScreen) {
+            Toast.makeText(this, "Clicked on Clear Screen", Toast.LENGTH_LONG).show();
+        } else if (view.getId() == R.id.PlusOrMinus) {
+            Toast.makeText(this, "Clicked on Plus or Minus", Toast.LENGTH_LONG).show();
+        } else if (view.getId() == R.id.Modulus) {
+            Toast.makeText(this, "Clicked on modulus", Toast.LENGTH_LONG).show();
+        } else if (view.getId() == R.id.Division) {
+            Toast.makeText(this, "Clicked on division", Toast.LENGTH_LONG).show();
+        } else if (view.getId() == R.id.Multiplication) {
+            Toast.makeText(this, "Clicked on multiplication", Toast.LENGTH_LONG).show();
+        } else if (view.getId() == R.id.Subtraction) {
+            Toast.makeText(this, "Clicked on Subtraction", Toast.LENGTH_LONG).show();
+        } else if (view.getId() == R.id.EqualSign) {
+            Toast.makeText(this, "Clicked on Equal Sign", Toast.LENGTH_LONG).show();
         }
     }
+
 
     public void numberClick(View view){
         Button clickedButton = findViewById(view.getId());
